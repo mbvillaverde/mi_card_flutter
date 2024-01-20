ea8ff5c46ed9a006c5e3fc85b32b2cfc19ab5ce7
Michael Villaverde <michaelv@qed.software>
2024-01-20 09:58:46 +0800

    done with first exercise

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: _flutterExercise1(),
+        ),
       ),
+    );
+  }
+
+  Row _flutterExercise1() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          width: 100,
+          color: Colors.red,
+        ),
+        Container(
+          width: 100.0,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 100.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                height: 100.0,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 100.0,
+          color: Colors.blue,
+        ),
+      ],
     );
   }
 }
